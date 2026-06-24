2ad9e930d4d050522077445b7e50d3703310dbc6
shivaranjani19 <sivaranjanik23ec@psnacet.edu.in>
2026-06-24 14:47:24 +0530

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -124,3 +124,23 @@ class array7{
         System.out.println("The second largest number is :" + secondlargest);
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int n = arr.length;
+        Solution obj = new Solution();
+        // Output result
+        System.out.println(obj.isSorted(arr, n) ? "True" : "False");
+    }
+}
+class Solution {
+    // Function to check if the array is sorted
+    public boolean isSorted(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i - 1])  // If any element is smaller than the previous one, return false
+                return false;
+        }
+        return true;  // Return true if the array is sorted
+    }
+}
