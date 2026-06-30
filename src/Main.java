0a91f1b7ece65d7aa56c0c1ddeee44f374c3bc10
shivaranjani19 <sivaranjanik23ec@psnacet.edu.in>
2026-06-30 12:21:41 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -128,19 +128,16 @@ class array7{
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        int n = arr.length;
         Solution obj = new Solution();
-        // Output result
-        System.out.println(obj.isSorted(arr, n) ? "True" : "False");
+        System.out.println(obj.isSorted(arr) ? "True" : "False");
     }
 }
 class Solution {
-    // Function to check if the array is sorted
-    public boolean isSorted(int[] arr, int n) {
-        for (int i = 1; i < n; i++) {
-            if (arr[i] < arr[i - 1])  // If any element is smaller than the previous one, return false
+    public boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1])
                 return false;
         }
-        return true;  // Return true if the array is sorted
+        return true;
     }
 }
